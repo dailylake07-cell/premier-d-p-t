aee467b304ad7144cde278da6c54248cc22297bd
dailylake07-cell <dailylake07@gmail.com>
2026-02-27 09:52:45 +0100

    modif
    ajout de la fonction multiplication et division

--- Operations.cpp
+++ Operations.cpp
@@ -17,3 +17,19 @@ int soustraction(int a, int b)
 	
 	return resultat;
 }
+int multiplication(int a, int b)
+{
+	int resultat;
+	
+	resultat = a * b;
+	
+	return resultat;
+}
+int divition(int a, int b)
+{
+	int resultat;
+	
+	resultat = a / b;
+	
+	return resultat;
+}
